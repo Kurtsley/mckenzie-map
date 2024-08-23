1ca4d015e73739131e732085bdbf18f97bad790c
Kurtsley <kurtsley@gmail.com>
2024-08-22 22:58:04 -0500

    re-add files

---
+++ app.py
@@ -0,0 +1,14 @@
+# Brian Beard 2024
+
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    image_url = url_for('static', filename='legend/resilist_1.png')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
